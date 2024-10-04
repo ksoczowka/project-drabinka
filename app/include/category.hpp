009d318644a218fcde88763ace23574aaae970ff
ksoczowka <ksoczowka.ks@gmail.com>
2024-10-04 12:17:42 +0200

    added addParticipant declaration

--- app/include/category.hpp
+++ app/include/category.hpp
@@ -1,12 +1,16 @@
 #pragma once
 
+#include "participant.hpp"
+
 #include <string>
+#include <vector>
 
 class Category {
 public:
     Category(const std::string& code);
 
-    void addParticipant();
+    void addParticipant(Participant& participant);
 private:
     std::string code_;
+    std::vector<Participant> participants_;
 };
