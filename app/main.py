dcb10994a4b9eb3d50513f142f640b63ec9ae8d5
ksoczowka <ksoczowka.ks@gmail.com>
2024-10-04 13:59:09 +0200

    new use case with new functions

--- app/main.py
+++ app/main.py
@@ -2,6 +2,17 @@ import ProjectDrabinka as PD
 
 path = "DATA.tsv"
 
+# makes Tournament object from file
 tournament = PD.Tournament(path)
 
-print(tournament.getAllParticipants())
+age_range = PD.RangeUnShort()
+age_range.min = 12
+age_range.max = 13
+
+weight_range = PD.RangeUnShort()
+weight_range.min = 0
+weight_range.max = 55
+
+tournament.addCategory("M12-1355-", age_range, weight_range, PD.Sex.MAN)
+
+print(tournament.getCategoryByCode("M12-1355-"))
