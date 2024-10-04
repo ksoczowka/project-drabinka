6202bfa6b0d578e2782761a5d6b7caed6b6c23d4
ksoczowka <ksoczowka.ks@gmail.com>
2024-10-04 14:03:30 +0200

    added getters and documentation for Participants

--- app/include/participant.hpp
+++ app/include/participant.hpp
@@ -7,11 +7,59 @@ enum class Sex {
     WOMAN
 };
 
+/**
+ * @class Participant
+ * @brief Class that handles operations regarding participants
+ */
 class Participant {
 public:
-    Participant(const unsigned int& id, const std::string& firstName, std::string& lastName, const unsigned short& age, const unsigned short& weight, const Sex& sex);
+    Participant() = delete;
+    /**
+     * @brief Constructor with all fields
+     * @param id ID of the participant
+     * @param firstName First name of the participant
+     * @param lastName Last name of the participant
+     * @param age Age of the participant
+     * @param weight Weight of the participant
+     * @param sex Sex of the participant taken from Sex enum
+     */
+    Participant(const unsigned int& id, const std::string& firstName, const std::string& lastName, const unsigned short& age, const unsigned short& weight, const Sex& sex);
 
+    /**
+     * @brief Gets ID
+     * @return ID of the participant
+     */
     unsigned int getID() { return id_; }
+    /**
+     * @brief Gets first name 
+     * @return first name of the participant
+     */
+    std::string getFirstName() { return firstName_; }
+    /**
+     * @brief Gets last name
+     * @return last name of the participant
+     */
+    std::string getLastName() { return lastName_; }
+    /**
+     * @brief Gets Name
+     * @return first name and last name combined in one string
+     */
+    std::string getName() { return firstName_ + " " + lastName_; }
+    /**
+     * @brief Gets age
+     * @return age of the participant
+     */
+    unsigned short getAge() { return age_; }
+    /**
+     * @brief Gets weight
+     * @return weight of the participant
+     */
+    unsigned short getWeight() { return weight_; }
+    /**
+     * @brief Gets sex
+     * @return Sex of the participant taken from Sex enum
+     */
+    Sex getSex() { return sex_; }
 private:
     unsigned int id_;
     std::string firstName_;
