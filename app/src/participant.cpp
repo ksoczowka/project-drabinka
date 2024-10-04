fb13935747791eb549ec655b792754bd7fb76e3b
ksoczowka <ksoczowka.ks@gmail.com>
2024-10-04 14:02:18 +0200

    added const to last name

--- app/src/participant.cpp
+++ app/src/participant.cpp
@@ -1,6 +1,6 @@
 #include "../include/participant.hpp"
 
-Participant::Participant(const unsigned int& id, const std::string& firstName, std::string& lastName, const unsigned short& age, const unsigned short& weight, const Sex& sex)
+Participant::Participant(const unsigned int& id, const std::string& firstName, const std::string& lastName, const unsigned short& age, const unsigned short& weight, const Sex& sex)
                         : id_(id), firstName_(firstName), lastName_(lastName), age_(age), weight_(weight), sex_(sex) {
 
 }
