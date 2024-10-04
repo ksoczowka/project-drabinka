e5f45cb2325335155d3da3f59a2c8aa76f2b9a92
ksoczowka <ksoczowka.ks@gmail.com>
2024-10-04 13:59:46 +0200

    made simple range structure template

---
+++ app/include/range.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+/**
+ * @struct Range
+ * @brief Simple range structure for easier range checking
+ */
+template<typename T>
+struct Range {
+    Range() {}
+    T min;
+    T max;
+
+    /**
+     * @brief Checks if value is in range
+     * @param value value to be checked if in range (including min and max)
+     */
+    bool isInRange(T value) {
+        if(value > max) return false;
+        if (value < min) return false;
+        return true;
+    }
+};
