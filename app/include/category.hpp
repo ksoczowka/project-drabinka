b7343c866c41c2b43bed5c05644545f79971fae6
ksoczowka <ksoczowka.ks@gmail.com>
2024-10-04 14:02:59 +0200

    added range structure to category for managing ranges of age and weight

--- app/include/category.hpp
+++ app/include/category.hpp
@@ -1,16 +1,22 @@
 #pragma once
 
 #include "participant.hpp"
+#include "range.hpp"
 
 #include <string>
 #include <vector>
 
 class Category {
 public:
-    Category(const std::string& code);
+    Category() = delete;
+    Category(const std::string& code, Range<unsigned short> ageRange, Range<unsigned short> weightRange, Sex sexCategory);
 
     void addParticipant(Participant& participant);
 private:
     std::string code_;
+    Range<unsigned short> ageRange_;
+    Range<unsigned short> weightRange_;
+    Sex sexCategory_;
+
     std::vector<Participant> participants_;
 };
