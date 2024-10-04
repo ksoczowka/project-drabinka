32431a0a68b2bdd3136f65c5622c2ddb5a725590
ksoczowka <ksoczowka.ks@gmail.com>
2024-10-04 12:17:56 +0200

    added bindings for other classes

--- app/src/bindings.cpp
+++ app/src/bindings.cpp
@@ -5,7 +5,12 @@
 
 PYBIND11_MODULE(ProjectDrabinka, m) {
     m.doc() = "Project Drabinka backend";
+    // Tournament class binding
     pybind11::class_<Tournament>(m, "Tournament")
     .def(pybind11::init<std::string>())
     .def("getAllParticipants", &Tournament::getAllParticipants, "Gets participants as a dictionary");
+    // Category class binding
+    pybind11::class_<Category>(m, "Category");
+    // Participant class binding
+    pybind11::class_<Participant>(m, "Participant");
 }
