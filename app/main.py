a658ea9c4ae15afdfc700935b3ee9df0ddbbedbd
ksoczowka <ksoczowka.ks@gmail.com>
2024-10-04 22:19:04 +0200

    RangeUnShort uses new constructor now in main.py

--- app/main.py
+++ app/main.py
@@ -5,13 +5,8 @@ path = "DATA.tsv"
 # makes Tournament object from file
 tournament = PD.Tournament(path)
 
-age_range = PD.RangeUnShort()
-age_range.min = 12
-age_range.max = 13
-
-weight_range = PD.RangeUnShort()
-weight_range.min = 0
-weight_range.max = 55
+age_range = PD.RangeUnShort(12, 13)
+weight_range = PD.RangeUnShort(0, 55)
 
 tournament.addCategory("M|12-13|0-55", age_range, weight_range, PD.Sex.MAN)
 
